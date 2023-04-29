c239f956b3259beda4226efd6a136e80c420635e
SayeedMahdi <sayeedmahdimousavi789@gmail.com>
2023-04-29 18:48:32 +0430

    reset password

--- utils/uploadCloudinary.js
+++ utils/uploadCloudinary.js
@@ -10,23 +10,61 @@ const uploadToCloud = async (req, res, next) => {
 	req.body.images = []
 
 	try {
-		Promise.all(
-			Object.values(files).map(async (file) => {
-				file.forEach(async (element) => {
-					const img = await cloudinary.cloudinary.uploader.upload(
-						element.path,
-						{
-							upload_preset: "leaninbox",
-						}
-					)
-					req.body.images.push({ ...img, name: element.fieldname })
-					console.log("read")
-					// await unlink(`${__dirname}/public/uploads/${element?.filename}`)
+		if (files && Object.values(files).length > 0) {
+			await Promise.all(
+				Object.values(files).map(async (file) => {
+					for (const element of file) {
+						const img = await cloudinary.cloudinary.uploader.upload(
+							element.path,
+							{
+								upload_preset: "leaninbox",
+							}
+						)
+						req.body.images.push({ ...img, name: element.fieldname })
+
+						await unlink(`${__dirname}/public/uploads/${element?.filename}`)
+					}
 				})
-			})
-		).then(next())
+			)
+		}
+
+		next()
 	} catch (err) {
 		next(err)
 	}
 }
+
 export default uploadToCloud
+
+// import path from "path"
+// import { unlink } from "fs/promises"
+// import cloudinary from "../config/cloudinary.js"
+
+// const uploadToCloud = async (req, res, next) => {
+// 	const __dirname = path.resolve()
+
+// 	const { files } = req
+
+// 	req.body.images = []
+
+// 	try {
+// 		Promise.all(
+// 			Object.values(files).map(async (file) => {
+// 				file.forEach(async (element) => {
+// 					const img = await cloudinary.cloudinary.uploader.upload(
+// 						element.path,
+// 						{
+// 							upload_preset: "leaninbox",
+// 						}
+// 					)
+// 					req.body.images.push({ ...img, name: element.fieldname })
+// 					console.log("read")
+// 					// await unlink(`${__dirname}/public/uploads/${element?.filename}`)
+// 				})
+// 			})
+// 		).then(next())
+// 	} catch (err) {
+// 		next(err)
+// 	}
+// }
+// export default uploadToCloud
