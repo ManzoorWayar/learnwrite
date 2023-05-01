55a71309b29cba17a137e1c84ab77ef3f5e72cf6
manzoorWayar <softwareengineerwayar@gmail.com>
2023-05-01 15:51:16 +0430

    refactior the code for image uplidng

--- utils/imgUploader.js
+++ utils/imgUploader.js
@@ -6,7 +6,7 @@ import { body } from "express-validator";
 const storage = multer.diskStorage({
   destination(req, file, cb) {
     console.log(file);
-    cb(null, "public/uploads/");
+    cb(null, "uploads/");
   },
 
   filename(req, file, cb) {
